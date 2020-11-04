fb27a38a83452c79ba3cfcb4e788f49b886dde04
SumitomShome <sumitomshome@gmail.com>
2020-11-04 22:28:45 +0530

    [Sumitom] Add: Ability to connect to database

---
+++ EmployeePayrollServiceFolder/EmployeePayrollServiceFolder/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace EmployeePayrollServiceProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to employee payroll problem");
+            EmployeeRepo repo = new EmployeeRepo();
+            EmployeeModel employee = new EmployeeModel();
+            repo.GetAllEmployee();
+            Console.ReadKey();
+        }
+    }
+}
